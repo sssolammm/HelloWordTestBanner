efab394b0721a0825317f50b3398df4747b04941
David Benito <david.benito@grupoica.com>
2017-08-11 15:00:07 +0200

    banner working

--- app/src/main/java/com/hellowordtestbanner/hellowordtestbanner/MainActivity.java
+++ app/src/main/java/com/hellowordtestbanner/hellowordtestbanner/MainActivity.java
@@ -5,104 +5,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private AdView mAdView;
     private InterstitialAd mInterstitialAd;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MobileAds.initialize(this, "ca-app-pub-2261905453458376~7631096487");
+//        MobileAds.initialize(this, "ca-app-pub-2261905453458376~7631096487");
 
-//        mAdView = (AdView) findViewById(R.id.ad_View);
-//        AdRequest adRequest = new AdRequest.Builder().build();
-//        mAdView.loadAd(adRequest);
+//        FAKE
+        MobileAds.initialize(this, "ca-app-pub-5676983055182772~3154571525");
 
+        //BANNER AD
+
+
+        mAdView = (AdView) findViewById(R.id.ad_View);
+        AdRequest adRequestBanner = new AdRequest.Builder()
+                .build();
+
+        mAdView.loadAd(adRequestBanner);
+
+        //INTERSTITAL AD
         mInterstitialAd = new InterstitialAd(this);
 //        test - ca-app-pub-3940256099942544/1033173712
         mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
-        AdRequest adRequest = new AdRequest.Builder()
+//        mInterstitialAd.setAdUnitId("ca-app-pub-2261905453458376/5942787901");
+        AdRequest adRequestInterstitial = new AdRequest.Builder()
                 .build();
-        mInterstitialAd.loadAd(adRequest);
-
-//        if (!mInterstitialAd.isLoading() && !mInterstitialAd.isLoaded()) {
-//            AdRequest adRequest = new AdRequest.Builder().build();
-//            mInterstitialAd.loadAd(adRequest);
-//        }
-
-        mInterstitialAd.setAdListener(new AdListener(){
-            @Override
-            public void onAdLoaded(){
-                mInterstitialAd.show();
-            }
-        });
-
-        Button myButton = (Button) findViewById(R.id.retry_button);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                } else {
-                    AdRequest adRequest = new AdRequest.Builder()
-                            .build();
-                    mInterstitialAd.loadAd(adRequest);
-                    mInterstitialAd.show();
-
-                    Log.d("TAG", "The interstitial wasn't loaded yet.");
-                }
-            }
-        });
-
-//        mAdView.setAdListener(new AdListener() {
-//            @Override
-//            public void onAdLoaded() {
-//                // Code to be executed when an ad finishes loading.
-//                Log.i("Ads", "onAdLoaded");
-//            }
-//
-//            @Override
-//            public void onAdFailedToLoad(int errorCode) {
-//                // Code to be executed when an ad request fails.
-//                Log.i("Ads", "onAdFailedToLoad");
-//            }
-//
-//            @Override
-//            public void onAdOpened() {
-//                // Code to be executed when an ad opens an overlay that
-//                // covers the screen.
-//                Log.i("Ads", "onAdOpened");
-//            }
-//
-//            @Override
-//            public void onAdLeftApplication() {
-//                // Code to be executed when the user has left the app.
-//                Log.i("Ads", "onAdLeftApplication");
-//            }
-//
-//            @Override
-//            public void onAdClosed() {
-//                // Code to be executed when when the user is about to return
-//                // to the app after tapping on an ad.
-//                Log.i("Ads", "onAdClosed");
-//            }
-//        });
+        mInterstitialAd.loadAd(adRequestInterstitial);
 
         mInterstitialAd.setAdListener(new AdListener() {
             @Override
             public void onAdLoaded() {
-                // Code to be executed when an ad finishes loading.
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                }
                 Log.i("Ads", "onAdLoaded");
             }
 
@@ -130,6 +78,40 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("Ads", "onAdClosed");
             }
         });
+
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Log.i("Ads", "onAdLoaded");
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                Log.i("Ads", "onAdFailedToLoad");
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+                Log.i("Ads", "onAdOpened");
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+                Log.i("Ads", "onAdLeftApplication");
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+                Log.i("Ads", "onAdClosed");
+            }
+        });
     }
 
 
@@ -152,30 +134,30 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//    /** Called when leaving the activity */
-//    @Override
-//    public void onPause() {
-//        if (mAdView != null) {
-//            mAdView.pause();
-//        }
-//        super.onPause();
-//    }
-//
-//    /** Called when returning to the activity */
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-//        if (mAdView != null) {
-//            mAdView.resume();
-//        }
-//    }
-//
-//    /** Called before the activity is destroyed */
-//    @Override
-//    public void onDestroy() {
-//        if (mAdView != null) {
-//            mAdView.destroy();
-//        }
-//        super.onDestroy();
-//    }
+    /** Called when leaving the activity */
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    /** Called when returning to the activity */
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    /** Called before the activity is destroyed */
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
 }
